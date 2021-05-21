c44170cda47c38079fb5b596fc963534efa7a3a3
proger <dev.elop.02@mail.ru>
2021-05-21 07:17:04 +0300

    добавил вывод всех постов

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from . import models
 
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.Category, CategoryAdmin)
